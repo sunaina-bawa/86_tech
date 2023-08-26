ff9b576c830efeca7955d9a3728e2d72c06f5b2f
sunaina <bawasunaina6@gmail.com>
2023-08-26 20:00:46 +0530

    done

--- Frontend/my-app/src/Component/api.js
+++ Frontend/my-app/src/Component/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8999", // Replace with your backend API base URL
+  baseURL: "https://eight6-tech-backend.onrender.com", // Replace with your backend API base URL
 });
 
 export default instance;
